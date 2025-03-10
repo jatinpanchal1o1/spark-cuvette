15930cb0d2d51919699cdbfdb1f187d1d980800e
jatinpanchal1o1 <jatin1o1panchal@gmail.com>
2025-03-10 23:42:45 +0530

    add chages11

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MainLogo from "../assets/MainFrame.svg";
 
 function Header() {
   return (
     <header className="px-auto p-1 sm:px-2 text-base w-full leading-loose text-center text-white bg-white border border-solid border-neutral-200 rounded-full">
-      <div className="flex flex-wrap gap-5 justify-between py-1 w-full">
-        <a href="/" className="py-4"><img
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/0bd5a8183c6b980a045c4e7393a5b79ee9bdd6c9c3308d0295e42ab7cbb6aa68?placeholderIfAbsent=true&apiKey=261c62fc181c4ec0b7c1c0697600ce32"
+      <div className="flex flex-wrap gap-5 justify-between py-1 w-full customCsss">
+        <a href="/"><img
+          src={MainLogo}
           className="object-contain shrink-0 my-auto lg:max-w-full w-60"
           alt="Spark logo"
         /></a>
